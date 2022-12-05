e25746525cd3ca4f8052eef05f74441dd1f60677
Andreas <andreas95560@gmail.com>
2022-12-05 17:15:37 +0100

    auth

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import Navbar from './components/navbar/navbar';
+import Router from './views/route';
+import Footer from './components/footer/footer';
+
+function App() {
+
+  
+  return (
+    <>
+      <Navbar />
+      <Router />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
